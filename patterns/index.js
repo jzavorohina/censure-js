f5f3df9782728f5782d71fb200a856734e3acd3e
jzavorohina <jzavorohina@yandex.ru>
2024-09-18 12:50:15 +0300

    Move patterns to separate files, update patterns

---
+++ patterns/index.js
@@ -0,0 +1,9 @@
+const patternsRu = require('./patterns_ru');
+const patternsEn = require('./patterns_en');
+const replacePatternsRu = require('./replace_patterns_ru');
+
+module.exports = {
+    patternsRu: patternsRu.patterns_ru,
+    patternsEn: patternsEn.patterns_en,
+    replacePatternsRu: replacePatternsRu.replace_patterns_ru
+}
---
+++ patterns/patterns_en.js
@@ -0,0 +1,22 @@
+const patterns_en = [
+    'ass|arse', 'asshole|arsehole',
+    'bastard', 'berk', 'bitch|bitch[a-z]+', 'blighter', 'blimey', 'bollocks', 'bugger', 'bullshit', 'butt',
+    'cad', 'cock|[a-z]+cock|cock[a-z]+', 'crap', 'cunt',
+    'damn', 'dang', 'darn', 'douchebag', 'dick|dick[a-z]+', 'duffer', 'dumd',
+    'faggot|fag', 'fool', 'freak|freaking ', 'fuck|[a-z]+fucker|fucking| fucked',
+    'gay',
+    'hoe', 'homo', 'heck',
+    'idiot',
+    'jerk|jerking',
+    'knob[a-z]+',
+    'loser',
+    'motherf[a-z]+',
+    'nerd', 'nigger',
+    'pillock', 'plonker', 'poo|poop', 'prat', 'prick', 'pussy',
+    'rotter',
+    'shit|shits|[a-z]+shit|shit[a-z]+|[a-z]+shit[a-z]+', 'slut', 'stupid', 'suck|sucker', 'swive|swivel', 'swine',
+    'twat',
+    'wanker', 'whore'
+];
+
+exports.patterns_en = patterns_en;
